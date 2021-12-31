8d737b828c916f3b881b63e52b0a25ec42d2d43b
Cody Backus <codyback@outlook.com>
2021-12-31 18:26:01 -0500

    dynamically show recipes

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const recipes = [
-  {id: 'chicken-and-rice-casserole', name: 'Chicken and Rice Casserole'},
-  {id: 'quick-chicken-piccata', name: 'Quick Chicken Piccata'},
-];
-const recipesList = document.getElementById('recipesList');
+const recipes = [];
 
-for (let i = 0; i < recipes.length; i += 1) {
-  recipesList.innerHTML += `<li><a href='./recipes/${recipes[i].id}.html'>${recipes[i].name}</a></li>`;
+const populateRecipe = (recipeID) => {
+  const recipe = recipes.find(obj => obj.id === recipeID);
+
+  const recipeNameContent = document.getElementById('recipeName');
+  const recipeDescriptionContent = document.getElementById('recipeDescription');
+  const recipeImgContent = document.getElementById('recipeImg');
+
+  recipeNameContent.innerHTML = recipe.name;
+  recipeDescriptionContent.innerHTML = recipe.description;
+  recipeImgContent.src = recipe.image.src;
+  recipeImgContent.alt = recipe.image.alt;
 }
-console.log(recipes);
+
+// fetches the recipes json data 
+fetch('./recipes.json').then(res => {
+  return res.json();
+}).then(data => {
+  // body > nav > ul#recipesList
+  const recipesNavContent = document.getElementById('recipesList');
+
+  // loops through recipes
+  for (let i = 0; i < data.recipes.length; i++) {
+    const recipe = data.recipes[i];
+    recipes.push(recipe);
+
+    recipesNavContent.innerHTML += `<li><a href="#" onclick="populateRecipe('${recipe.id}')" id="recipe${recipe.id}">${recipe.name}</a></li>`;
+  }
+  
+  const randomRecipe = Math.floor(Math.random() * recipes.length);
+
+  populateRecipe(recipes[randomRecipe].id);  
+});
